b9cc998ecf8b8ca77491338715a3318ae90981cd
ahmetosan <ahmetpcf@gmail.com>
2022-05-07 15:02:52 -0400

    day 2 - updated end of the day

--- src/main/java/com/cydeo/service/impl/UserServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/UserServiceImpl.java
@@ -1,37 +1,80 @@
 package com.cydeo.service.impl;
 
 import com.cydeo.dto.UserDTO;
+import com.cydeo.entity.User;
+import com.cydeo.mapper.UserMapper;
+import com.cydeo.repository.UserRepository;
 import com.cydeo.service.UserService;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class UserServiceImpl implements UserService {
+
+   private final UserRepository userRepository;
+   private final UserMapper userMapper;
+
+    public UserServiceImpl(UserRepository userRepository, UserMapper userMapper) {
+        this.userRepository = userRepository;
+        this.userMapper = userMapper;
+    }
+
+
     @Override
     public List<UserDTO> listAllUser() {
 
-
-        return null;
+        List<User> userList = userRepository.findAll(Sort.by("firstName"));
+        return userList.stream().map(userMapper::convertToDto).collect(Collectors.toList());
     }
 
     @Override
     public UserDTO findByUserName(String userName) {
-        return null;
+
+        User user = userRepository.findByUserName(userName);
+        return userMapper.convertToDto(user);
     }
 
     @Override
     public void save(UserDTO dto) {
 
+        userRepository.save(userMapper.convertToEntity(dto));
+
+
     }
 
     @Override
     public UserDTO update(UserDTO dto) {
-        return null;
+
+        //Find current user
+        User user = userRepository.findByUserName(dto.getUserName());
+        //
+        User covertedUser = userMapper.convertToEntity(dto);
+
+        covertedUser.setId(user.getId());
+
+        userRepository.save(covertedUser);
+ //       userRepository.save(userMapper.convertToEntity(dto));
+
+        return  findByUserName(dto.getUserName());
     }
 
     @Override
     public void deleteByUserName(String username) {
+
+        userRepository.deleteByUserName(username);
+
+    }
+
+    @Override
+    public void delete(String username) {
+
+        User user = userRepository.findByUserName(username);
+        user.setIsDeleted(true);
+        userRepository.save(user);
+
 
     }
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO roles(description) VALUES ('ADMIN');
+INSERT INTO roles(description) VALUES ('MANAGER');
+INSERT INTO roles(description) VALUES ('PO');
+INSERT INTO roles(description) VALUES ('TESTER');
+INSERT INTO roles(description) VALUES ('DEVELOPER');
